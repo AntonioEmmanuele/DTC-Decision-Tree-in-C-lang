d96fc3a2d947f9dc3ee56f5357c13f337ae12dfc
AntonioVivoBook <antony.35.ae@gmail.com>
2024-12-27 15:35:11 +0100

    adding pygen

---
+++ dtc_pygen/dtc_pygen.py
@@ -0,0 +1,25 @@
+"""
+@file dtc_pygen.py
+@brief This file contains the utils to generate a binary configurator for the c lib given a PMML file in input.
+
+
+@copyright Copyright (C) 2024 Antonio Emmanuele
+
+This file is part of DTC Decision Tree in C-lang
+
+DTC is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+DTC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with DTC. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+if __name__ == "__main__":
+    print("Hello World")
